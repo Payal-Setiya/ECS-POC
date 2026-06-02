93cc222be09fb80d53e80c520e627a7c70bf31cd
Payal Setiya <setiya.payal1992@gmail.com>
2026-06-02 10:58:03 +0000

    Python POC

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    env_name = os.getenv("ENV_NAME", "dev")
+    return f"Python Flask App running on ECS Fargate - Environment: {env_name}"
+
+@app.route("/health")
+def health():
+    return jsonify(status="UP"), 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
